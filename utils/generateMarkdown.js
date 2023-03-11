34a1c4066dcc8926ca8bf29f65d9834178369941
Mo <mohammedzaber@hotmail.com>
2023-03-11 17:50:00 +0000

    added table of contents to the markdown that will be generated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,44 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projecttitle}
+
+  ## Description
+
+  ${data.projectdescription}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Tests](#tests)
+  * [Usage](#usage)
+  * [Collaborators](#collaborators)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.projectinstall}
+
+  ## Tests
+
+  ${data.projecttest}
+
+  ## Usage
+
+  ${data.projectuse}
+
+  ## Collaborators
+
+  ${data.projectcollaborator}
+
+    ## License
+
+  ${data.projectlicense}
+
+  ## Questions
+
+  ${data.username}
+  ${data.email}
 
 `;
 }
