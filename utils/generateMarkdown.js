842ee2c60cee6c9aacc1a81ae070a1f61935089b
Mo <mohammedzaber@hotmail.com>
2023-03-11 17:59:02 +0000

    License badge that matches the user's license now appears in markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
+  let projectLisenseBadge;
+
+  if (data.projectlicense === "MIT") {
+    projectLisenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  
+  } if (data.license === "Eclipse") {
+    projectLisenseBadge = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  
+  } if (data.license === "IBM") {
+    projectLisenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+  
+  } if (data.license === "ISC") {
+    projectLisenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+  
+  }if (data.license === "SIL") {
+    projectLisenseBadge = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
+  
+  }if (data.license === "None") {
+    projectLisenseBadge = "";
+  }
+
+
+
   return `# ${data.projecttitle}
 
   ## Description
@@ -35,6 +58,8 @@ function generateMarkdown(data) {
     ## License
 
   This project is covered under the ${data.projectlicense} license
+
+  ${projectLicenseBadge}
 
   ## Questions
 
