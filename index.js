73d344fb743bd0c8cdd1de8330bfb945bab412b5
Mo <mohammedzaber@hotmail.com>
2023-03-11 17:46:51 +0000

    added the array questions for the user

--- index.js
+++ index.js
@@ -5,7 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+        {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "projecttitle"
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "projectdescription"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "projectinstall"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "projecttest"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using your repo?",
+        name: "projectuse"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "projectcollaborator"
+    },
+        {
+        type: "list",
+        message: "What license should your project have?",
+        name: "projectlicense",
+        choices: ['None','MIT','Eclipse', 'IBM', 'ISC', 'SIL']
+    },
 ];
 
 // function to write README file
