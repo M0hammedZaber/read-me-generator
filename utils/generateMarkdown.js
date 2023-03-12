e6d2f80deb7771084ac686e823f3048b3ddccc72
Mo <mohammedzaber@hotmail.com>
2023-03-12 10:45:55 +0000

    tested, removed readme and will try again

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-  let projectLisenseBadge;
+  let licenseBadge;
 
   if (data.projectlicense === "MIT") {
-    projectLisenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
   
-  } if (data.license === "Eclipse") {
-    projectLisenseBadge = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  } if (data.projectlicense === "Eclipse") {
+    licenseBadge = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
   
-  } if (data.license === "IBM") {
-    projectLisenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+  } if (data.projectlicense === "IBM") {
+    licenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
   
-  } if (data.license === "ISC") {
-    projectLisenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+  } if (data.projectlicense === "ISC") {
+    licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
   
-  }if (data.license === "SIL") {
-    projectLisenseBadge = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
+  }if (data.projectlicense === "SIL") {
+    licenseBadge = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
   
-  }if (data.license === "None") {
-    projectLisenseBadge = "";
+  } if (data.projectlicense === "None") {
+    licenseBadge = "";
   }
 
 
@@ -55,11 +55,11 @@ function generateMarkdown(data) {
 
   ${data.projectcollaborator} contributed to this project
 
-    ## License
+  ## License
 
   This project is covered under the ${data.projectlicense} license
 
-  ${projectLicenseBadge}
+  ${licenseBadge}
 
   ## Questions
 
